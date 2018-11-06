7056ce0645fcaf323cc3c06802b3668be44ddefa
bard85 <44797174+bard85@users.noreply.github.com>
2018-11-06 13:05:19 +0100

    Add files via upload

--- Software/test/validate_gcode_program.py
+++ Software/test/validate_gcode_program.py
@@ -4,15 +4,16 @@ needed by the controller system on Arduino, GRBL1.1(https://github.com/gnea/grbl
 GRBL can't handle canned cycles native but we will post process this later so it is ok to
 use canned cycle's in a program
 """
-"""
-TODO
--check_number could use som love
-"""
-
 
 
 def validate_gcode_program(file_path):
-    warning_list = []       #Warnings are not a fatal error but 'good-to-know'
+    """
+    This function loop through an entire program-file.
+    Step by step it checks the syntax and modifies the string by removing
+    parts thats ok. If a line of code is completely empty by the end
+    it means that the line was ok. If we got some leftovers there is an error
+    with the line(unknown commands, missed comment signs etc..)
+    """
     error_list = []         #Errors mean something will go wrong during a run, will not let you run the program
     program_ok = True       #Should stay ok during the validation, if not an error was found
     last_x = 0.0            #Used for arc-calculations
@@ -24,54 +25,65 @@ def validate_gcode_program(file_path):
     try:
         with open(file_path, 'r') as f:
             for idx, line in enumerate(f):
-                #Cleaning up abit
-                line = line.strip().replace(" ","").replace("\t","").upper()
+                line = modify_line(line)
+                if len(line) == 0:
+                    continue
+
                 #Checking for comment(s)
                 status, line = check_comments(line)
                 if not status:
                     error_list.append([idx+1,line])
                     continue
+                if len(line) == 0:
+                    continue
+
                 #Checking line numbering
                 status, line = check_numbering(line)
                 if not status:
                     error_list.append([idx+1,line])
                     continue
-                #Text that are supposed to be removed are now gone
-                #If the line is empty, no need to check anymore..
                 if len(line) == 0:
                     continue
-                #If M-code, taking a look at that one
-                if 'M' in line:
-                    status, line = check_m_code(line, spindle_speed_set)
 
-
+                #Looking for M-codes
+                status, line, spindle_speed_set = check_m_code(line, spindle_speed_set)
+                if not status:
+                    error_list.append([idx+1,line])
+                    continue
+                if len(line) == 0:
+                    continue
 
         print(error_list)
     except Exception as e:
         #Something went wrong opening the file...
         print("failed", e)
 
+def modify_line(line):
+    #Cleaning up the line
+    line = line.strip().replace(" ","").replace("\t","").upper()
+    line = line.replace("M00","M0").replace("M02","M2").replace("M03","M3")
+    line = line.replace("M04","M4").replace("M05","M5").replace("M08","M8")
+    line = line.replace("M09","M9")
+    line = line.replace("G00","G0").replace("G01","G1").replace("G02","G2")
+    line = line.replace("G03","G3")
+    return line
+
+def get_value(line, instruction_idx):
+    #Function that parse the number value of instruction
+    value = line[instruction_idx+1:]
+    for idx, c in enumerate(value):
+        if c.isalpha():
+            value = value[:idx]
+            break
+    return value
 
 def check_comments(line):
-    """
-    Looking for comments in line and removes them.
-    Comments can be done in two ways:
-    -Inside brackets (this is a comment)
-    -Insert a ;, everything after is a comment
-    -Example 1: G01 X0 Y2 ;This is a comment
-    -Example 2: G01 (This is a comment) X0 Y0
-    -A comment can not be between a letter and number like this:
-    -G(This comment is failing)01 X0 Y2
-    -The amount of ; can't be > 1
-    """
-
-    #Starting to analyse ;-comments
+    #See documentation for the syntax used in this function
+    #or the comment.ngc file
+    
+    #Starting to analyse semi colon's
     if line.count(';') == 1:
-        #Found one, this is ok. Removing comment
         line = line[:line.find(';')]
-    elif line.count(';') > 1:
-        #Found more than one, not ok. Returning an error
-        return False, 1
     
     #Analyzing comments in brackets
     if line.count('(') != line.count(')'):
@@ -82,108 +94,99 @@ def check_comments(line):
             if line.find('(') > line.find(')'):
                 #The sit in the wrong order )(
                 return False, 1
+            if line.count('(') > 1:
+                if line[line.find('(')+1:].find('(') < line[line.find('(')+1:].find(')'):
+                   #Nesting (  (   ))
+                    return False, 1
             line = line[:line.find('(')]+line[line.find(')')+1:]
-
-    #Everything was fine
+    
     return True, line
 
 
 def check_numbering(line):
-    """
-    Looking for line numbering and removes them.
-    Syntax for line numbering:
-    -Starts with N followed by number(int)
-    -Number needs to be >=0
-    -N can only occur once
-    -N needs to be first char in line
-    """
+    #See documentation for the syntax used in this function
+    #or the line_numbering.ngc file
 
     if not line.count('N') > 0:
         #No N present, nothing to do anymore
         return True, line
+
     if line.count('N') > 1:
         #To many N, error...
         return False, 2
-    elif line[0] != 'N':
+
+    if line[0] != 'N':
         #N is in the wrong spot
         return False, 2
-    line = line[1:]
-    number_value = line
-    for idx, c in enumerate(line):
-        if c.isalpha():
-            number_value = line[:idx]
-            break
+    number_value = get_value(line, 0)
     if len(number_value) == 0 or '.' in number_value or int(number_value) <0:
         #No line number set, its a float or its negative
         return False, 2
-    elif len(number_value) == len(line):
+    elif len(number_value) == len(line)+1:
         #Only the number left, line is now empty
         return True, ""
     else:
-        line = line[idx:]
-        return True, line
+        line = line[len(number_value)+1:]
+    return True, line
 
 
 def check_m_code(line, spindle_speed_set):
-    """
-    Checking rows with M-code's in it.
-    Supported M-codes by the GRBL(1.1) is:
-    M0: Program stop
-    M2: Program End
-    M3: Start spindle CW
-    M4: Start spindle CCW
-    M5: Stop spindle
-    M7*: Mist Coolant on
-    M8: Flood coolant on
-    M9: Coolant off
-    M30: Program end(see M02) and return to program start
-    M56*: Parking mode
-    *Needs to be enabled during compiling to Arduino,
-    they will generate error for now!
-    -General syntax:
-    -M and G-codes can't be combined on same row
-    -Multiple M-codes not allowed
-    -M3, M4 can be set with S-parameter aswell
-    -M3, M4 needs a set speed(S) from before or same row
-    """
+    #After this function the return line should be empty
+    #If not its a failed check
     if line.count('M') > 1:
         #To many M's, error..
-        return False, 3
+        return False, 3, spindle_speed_set
     if 'G' in line:
         #Can't combine M's & G's...
-        return False, 4
-    #Shortening string up abit
-    line = line.replace("M00","M0").replace("M02","M2").replace("M03","M3")
-    line = line.replace("M04","M4").replace("M05","M5").replace("M08","M8")
-    line = line.replace("M09","M9")
-    #Checking codes with no optional args
+        return False, 3, spindle_speed_set
+    
+    #Checking codes with no optional arguments
     m_no_args = "M0","M2","M5","M8","M9","M30"
     for m in m_no_args:
-        if line == m:
-            return True, line
+        if m in line:
+            line = line.replace(m,"")
+            if len(line) == 0:
+                return True, "", spindle_speed_set
+            else:
+                return False, 3, spindle_speed_set
+    
     #Still here? M-code now should be spindle start, else unknown..
     if "M3" in line or "M4" in line:
         #We got spindle start, what about speed?
         if line == "M3" or line == "M4":
             #Speed needs to be set before, lets hope so
             if spindle_speed_set:
-                #puh, we ok
-                return True, line
+                return True, "", spindle_speed_set
             else:
-                #No speed set from before, cant start...
-                return False, 5
+                return False, 3, spindle_speed_set
         else:
             #Ok, we got spindle M3/4 and some more, lets hope its an S
             if "S" in line:
-                pass
+                if line.count('S') > 1:
+                    return False, 3, spindle_speed_set
+                else:
+                    number_value = get_value(line, line.find('S'))
+                    if len(number_value) == 0 or float(number_value) <0:
+                        #No speed set its negative
+                        return False, 3, spindle_speed_set
+                    else:
+                        line = line.replace("M3","").replace("M4","")
+                        line = line.replace("S"+number_value,"")
+                        if len(line) == 0:
+                            return True, "", True
+                        else:
+                            #Still got something in string that shouldnt be there
+                            return False, 3, spindle_speed_set
             else:
                 #No speed in here..
-                return False, 5
-    
-    return True, line
+                return False, 3, spindle_speed_set
+    else:
+        #Got a unsupported M-code here or some weird syntax for Mxx
+        return False, 3, spindle_speed_set
 
-
-validate_gcode_program("test.gcode")
+#validate_gcode_program("Example files/comments.ngc")
+#validate_gcode_program("Example files/line_numbering.ngc")
+validate_gcode_program("Example files/m_codes.ngc")
 
 
 
@@ -191,8 +194,6 @@ validate_gcode_program("test.gcode")
 Validation errors:
 1: Fel syntax för kommentar
 2: Fel syntax för radnumrering
-3: Fel syntax för M-kod. För många M-instruktioner på samma rad
-4: Fel syntax för M-kod. M och G på samma rad
-5: Fel syntax för M-kod. Försöker starta spindel utan att hastighet är definierad
+3: Fel syntax för M-kod
 """
 
